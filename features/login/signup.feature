589ea37cbec1d6930efce1bdbf8627ceacea7238
cyrusaf <forbescyrus@gmail.com>
2014-07-28 17:07:50 +0000

    Added test for signup

--- features/login/signup.feature
+++ features/login/signup.feature
@@ -1,4 +1,5 @@
 Feature: Sign up a new user
     Scenario:
+        Given table users is empty
         When I submit a form called signup with username cyrusaf and password bugzzues
-        Then I have a user with username cyrusaf and password bugzzues
+        Then I can login with username cyrusaf and password bugzzues
--- features/step_definitions/login.rb
+++ features/step_definitions/login.rb
@@ -1,14 +1,38 @@
 require 'net/http'
+require_relative '../../models/models'
 
-Given /^When I submit a form called signup with username (\w+) and password (\w+)$/ do |username, password|
+Given(/^table (\w+) is empty$/) do |table_name|
+    ActiveRecord::Base.connection.execute("TRUNCATE #{table_name}")
+end
+
+When(/^I submit a form called signup with username (\w+) and password (\w+)$/) do |username, password|
     url = URI.parse("http://localhost:4567")
     http = Net::HTTP.new(url.host, url.port)
     req = Net::HTTP::Post.new('/users/new')
-    req.add_field('Content-Type', 'application/json')
-    req.body = {
-        'username' => 'cyrusaf5',
-        'password' => 'bugzzues'
-    }.to_s
+    req.set_form_data({
+        'username' => username,
+        'password' => password,
+        'first_name' => 'Cyrus',
+        'last_name' => 'Forbes'
+    })
     res = http.request(req)
-    puts res.body
+    puts res.code
+    if res.code != '200'
+        fail(StandardError.new("Status code: #{res.code}")) 
+    end
+end
+
+Then(/^I can login with username (\w+) and password (\w+)$/) do |username, password|
+    url = URI.parse("http://localhost:4567")
+    http = Net::HTTP.new(url.host, url.port)
+    req = Net::HTTP::Post.new('/login')
+    req.set_form_data({
+        'username' => username,
+        'password' => password
+    })
+    res = http.request(req)
+    puts res.code
+    if res.code != '200'
+        fail(StandardError.new("Status code: #{res.code}")) 
+    end
 end
